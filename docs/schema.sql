c34bf7889b2577b5d31372bf06e5648a6f7a02cb
Umer <l233085@lhr.nu.edu.pk>
2026-07-20 23:04:54 +0500

    finalize schema

--- docs/schema.sql
+++ docs/schema.sql
@@ -10,6 +10,7 @@ create table admin (
 create table category (
 	id bigint unsigned not null auto_increment,
     title varchar(255) not null,
+    description text,
     primary key category_id (id),
     unique key category_title (title)
 );
@@ -18,6 +19,7 @@ create table subcategory (
 	id bigint unsigned not null auto_increment,
     title varchar(255) not null,
     category_id bigint unsigned not null,
+    description text,
     primary key subcategory_id (id),
     unique key subcategory_title (title),
     constraint foreign_category_id foreign key ( category_id ) references category ( id ) On delete cascade on update cascade
@@ -38,43 +40,56 @@ create table article_videos (
 create table surah (
 	surah_number tinyint unsigned not null,
     total_ayat smallint unsigned not null,
-    arabic text,
-    primary key surah_no (surah_number)
+    name_arabic varchar(100) not null,
+    english_name varchar(100) not null,
+    english_name_translation varchar(100) not null,
+    relevation_type Enum('Meccan','Medinan') not null,
+    primary key surah_no (surah_number),
+    unique key surah_names (name_arabic)
 );
 
 create table verse (
-	verse_number int unsigned not null,
+	number_in_surah int unsigned not null,
     arabic text,
     surah_number tinyint unsigned not null,
-    primary key verse_no (verse_number),
+    global_number int unsigned unique not null,
+    juz tinyint unsigned,
+    manzil tinyint unsigned,
+    page_no smallint unsigned,
+    ruku smallint unsigned,
+    hizb_quarter tinyint unsigned,
+    sajda boolean default false,
+    primary key verse_identifier (surah_number,number_in_surah),
     constraint foreign_surah_no foreign key ( surah_number ) references surah ( surah_number ) on delete cascade on update cascade
 );
 
-create table tafseer_detail (
-	id bigint unsigned not null auto_increment,
+create table translation_tafseer_details (
+    id bigint unsigned not null auto_increment,
+    title varchar(255) not null,
+    lang char(2) not null,     
     author varchar(100) not null,
-    name varchar(100) not null,
+    direction Enum('ltr','rtl'),
     description text,
-    primary key tafseer_detail_id ( id )
+    primary key tafseer_detail_id (id),
+    unique key translation_tafseer_title ( title ) 
 );
 
-create table translation_tafseer (
-	id bigint unsigned not null auto_increment,
-    translation longtext,
-    tafseer longtext,
-    lang varchar(10) not null,
-    surah_no tinyint unsigned not null,
-    verse_no int unsigned not null,
-    tafseer_id bigint unsigned not null,
-    primary key translation_tafseer_id ( id ),
-    constraint foreign_surah_no_for_detail foreign key ( surah_no ) references surah ( surah_number ) on delete cascade on update cascade,
-    constraint foreign_verse_no foreign key ( verse_no ) references verse ( verse_number ) on delete cascade on update cascade,
-    constraint foreign_tafseer_id foreign key ( tafseer_id ) references tafseer_detail ( id ) on delete cascade on update cascade
-);    
+create table verse_texts (
+	surah_number tinyint unsigned not null,
+    verse_number int unsigned not null,
+    detail_id bigint unsigned not null,
+    verse_translation longtext not null,
+    verse_tafseer longtext,
+    primary key verse_text_id (surah_number,verse_number),
+    constraint foreign_surah_and_verse_no_for_text foreign key ( surah_number,verse_number ) references verse ( surah_number,number_in_surah ) on delete cascade on update cascade,
+    constraint verse_texts_identifier foreign key (detail_id) references translation_tafseer_details (id) on delete cascade on update cascade
+);
 
 show tables
-drop tables admin,article_videos,
-drop tables translation_tafseer
-drop tables tafseer_detail
-drop tables verse
-drop tables surah,subcategory,category
+drop table verse_texts;
+drop table verse;
+drop table surah;
+drop table translation_tafseer_details;
+drop table article_videos;
+drop table admin,subcategory;
+drop table category;
