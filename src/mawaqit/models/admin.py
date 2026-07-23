6db01fed2a0269772b2a9256af729062e4846af4
Umer <l233085@lhr.nu.edu.pk>
2026-07-23 15:38:56 +0500

    admin model added

---
+++ src/mawaqit/models/admin.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, String
+from mawaqit.database import Base
+
+
+class Admin(Base):
+    __tablename__ = "admin"
+    username = Column(String(100), primary_key=True, unique=True, nullable=False)
+    password_hash = Column(String(255), nullable=False)
