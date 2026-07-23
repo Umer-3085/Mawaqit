0a89c724a9f327fd35bf0243e369da94657cde51
Umer <l233085@lhr.nu.edu.pk>
2026-07-23 18:47:15 +0500

    article_videos router in main added

--- src/mawaqit/api/main.py
+++ src/mawaqit/api/main.py
@@ -1,10 +1,8 @@
 from fastapi import APIRouter
-
 from mawaqit.api.admin.router import router as admin_router
-
 from mawaqit.api.category import category_router
-
 from mawaqit.api.subcategory import subcategory_router
+from mawaqit.api.article_videos import article_video_router
 
 api_router = APIRouter(prefix="/api")
 
@@ -13,3 +11,5 @@ api_router.include_router(admin_router)
 api_router.include_router(category_router)
 
 api_router.include_router(subcategory_router)
+
+api_router.include_router(article_video_router)
