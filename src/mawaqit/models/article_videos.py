537999f90402864ef4ba3ef06dded92164edec13
Umer <l233085@lhr.nu.edu.pk>
2026-07-23 18:40:53 +0500

    article_videos model added

---
+++ src/mawaqit/models/article_videos.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, BigInteger, String, Text, ForeignKey
+from mawaqit.database import Base
+
+class ArticleVideo(Base):
+    __tablename__ = "article_videos"
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    title = Column(String(500), nullable=False)
+    detail = Column(Text, nullable=True)
+    category_id = Column(BigInteger, ForeignKey("category.id", ondelete="RESTRICT", onupdate="CASCADE"), nullable=False)
+    subcategory_id = Column(BigInteger, ForeignKey("subcategory.id", ondelete="SET NULL", onupdate="CASCADE"), nullable=True)
+    link = Column(String(1000), nullable=True)
