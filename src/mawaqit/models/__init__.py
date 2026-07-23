f9f2264aa92d8778bd43b35aeeaa6f253881532f
Umer <l233085@lhr.nu.edu.pk>
2026-07-23 18:57:34 +0500

    article_videos import added in model import

--- src/mawaqit/models/__init__.py
+++ src/mawaqit/models/__init__.py
@@ -1,9 +1,11 @@
 from .admin import Admin
 from .category import Category
 from .subcategory import SubCategory
+from .article_videos import ArticleVideo
 
 __all__ = [
     "Admin",
     "Category",
     "SubCategory",
+    "ArticleVideo",
 ]
