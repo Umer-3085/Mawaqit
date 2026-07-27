a4bd3ae336dc8bd5d4c8a560b79fe93773d6fb80
Umer <l233085@lhr.nu.edu.pk>
2026-07-27 21:19:36 +0500

    details import added

--- src/mawaqit/models/__init__.py
+++ src/mawaqit/models/__init__.py
@@ -2,10 +2,12 @@ from .admin import Admin
 from .category import Category
 from .subcategory import SubCategory
 from .article_videos import ArticleVideo
+from .translation_tafseer_details import TranslationTafseerDetailRepository
 
 __all__ = [
     "Admin",
     "Category",
     "SubCategory",
     "ArticleVideo",
+    "TranslationTafseerDetailRepository"
 ]
