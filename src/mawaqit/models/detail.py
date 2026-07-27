0b95dee228e87cf9879eb6ceb5ce51b0f2242474
Umer <l233085@lhr.nu.edu.pk>
2026-07-27 21:11:01 +0500

    details model added

---
+++ src/mawaqit/models/detail.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String, Text, Enum
+from sqlalchemy.dialects.mysql import BIGINT, TINYINT, CHAR
+from mawaqit.database import Base
+
+class TranslationTafseerDetail(Base):
+    __tablename__ = "translation_tafseer_details"
+    id = Column(BIGINT(unsigned=True), primary_key=True, autoincrement=True)
+    title = Column(String(255), nullable=False, unique=True)
+    lang = Column(CHAR(2), nullable=False)
+    author = Column(String(100), nullable=False)
+    direction = Column(Enum('ltr', 'rtl', name='direction_enum'), nullable=True)
+    description = Column(Text, nullable=True)
