e26c28b765acedd279eedc92c521375459652dd7
Umer <l233085@lhr.nu.edu.pk>
2026-07-27 21:43:05 +0500

    add surah and verse import plus complete detail module along with fully tested apis

--- src/mawaqit/models/__init__.py
+++ src/mawaqit/models/__init__.py
@@ -2,12 +2,14 @@ from .admin import Admin
 from .category import Category
 from .subcategory import SubCategory
 from .article_videos import ArticleVideo
-from .detail import TranslationTafseerDetailRepository
+from .detail import TranslationTafseerDetail
 
 __all__ = [
     "Admin",
     "Category",
     "SubCategory",
     "ArticleVideo",
-    "TranslationTafseerDetailRepository"
+    "Surah",
+    "Verse",
+    "TranslationTafseerDetail"
 ]
